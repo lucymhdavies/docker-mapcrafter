73c9a0a8dc7b5f2ee27809ef45bbd15a2f1f2e12
Lucy Davies <lucy@lucymhdavies.com>
2017-05-12 11:06:27 +0000

    Hacky download script, until I figure out proper ftp mirroring

--- download.sh
+++ download.sh
@@ -7,5 +7,9 @@ set -ex
 mkdir -p /data/worlds/Davinhart\ Kingdom || true
 cd /data/worlds/Davinhart\ Kingdom
 
-wget --cut-dirs=1 -c -N -nH -m ftp://$MC_USER:$MC_PASS@$MC_IP//world
+# Temporary hack to ensure that the downloaded world is up-to-date
+rm -rf *
+
+#wget --cut-dirs=1 -c -N -nH -m ftp://$MC_USER:$MC_PASS@$MC_IP//world
+wget --cut-dirs=1 -c -nH -r -l inf --no-remove-listing ftp://$MC_USER:$MC_PASS@$MC_IP//world
 
